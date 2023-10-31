a6d99d75b192336181bc77ff94c112863c330398
amenov <amenov.abdulsalam@gmail.com>
2023-10-31 17:53:28 +0600

    update

--- commands/make/migrations.js
+++ commands/make/migrations.js
@@ -1,8 +1,19 @@
 module.exports = {
   command: 'make:migrations',
-  describe: '',
+  describe: 'Generate database migration files based on module models',
   builder(yargs) {
-    yargs.example([['$0 make:migrations']])
+    yargs.option('module', { alias: 'm', describe: 'Module name', demandOption: true, type: 'string' })
+    yargs.option('model', { describe: 'Model name', type: 'string' })
+
+    if (yargs.argv.model) {
+      yargs.demandOption('module', 'Module name must be provided when generating migrations for a specific model')
+    }
+
+    yargs.example([
+      ['$0 make:migrations'],
+      ['$0 make:migrations --module users'],
+      ['$0 make:migrations --module users --model User']
+    ])
 
     return yargs
   },
