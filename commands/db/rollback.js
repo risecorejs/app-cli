67db85ec9a09ce29ec03600235b577618e619bed
amenov <amenov.abdulsalam@gmail.com>
2023-10-31 17:36:15 +0600

    update

--- commands/db/rollback.js
+++ commands/db/rollback.js
@@ -1,8 +1,26 @@
 module.exports = {
-  command: 'db:rollback',
-  describe: '',
+  command: 'db:rollback [file]',
+  describe: 'Rollback the last database migration files of modules',
   builder(yargs) {
-    yargs.example([['$0 db:rollback']])
+    yargs.option('file', { alias: 'f', describe: 'Migration file', type: 'string' })
+    yargs.option('module', { alias: 'm', describe: 'Module name', type: 'string' })
+
+    if (yargs.argv.file) {
+      yargs.demandOption('module', 'Module name must be provided when rolling back a single file')
+    }
+
+    yargs.example([
+      ['$0 db:rollback', 'Rollback the last database migration for all modules'],
+      ['$0 db:migrate --module users', 'Rollback the last database migration for a specific module'],
+      [
+        '$0 db:migrate 0001_20231029_create_table_users.js --module users',
+        'Rollback a specific migration file for a module'
+      ],
+      [
+        '$0 db:migrate --file 0001_20231029_create_table_users.js --module users',
+        'Rollback a specific migration file for a module'
+      ]
+    ])
 
     return yargs
   },
